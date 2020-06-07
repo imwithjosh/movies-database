78e07e13345e8503b712c181a00cc5e6772cb6e3
oyewale tayo <tayooyewale@gmail.com>
2020-06-07 01:59:47 +0100

    depolying site

---
+++ precache-manifest.ef3b33080e132d892e56b75b647d1713.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99783614c44e515c9d820c321b2f3b2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "521706dd7ff44e4c2c72",
+    "url": "/static/css/main.267384fb.chunk.css"
+  },
+  {
+    "revision": "9a525e00754e22c0af10",
+    "url": "/static/js/2.8d689980.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8d689980.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "521706dd7ff44e4c2c72",
+    "url": "/static/js/main.c86a173d.chunk.js"
+  },
+  {
+    "revision": "87fde591fead47f617e7",
+    "url": "/static/js/runtime-main.0818f1db.js"
+  }
+]);
